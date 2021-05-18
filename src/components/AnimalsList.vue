9d0e117361f17e52bcba9b1f8facb7c281197363
Sezana FIlipovic <sezanafilipovic@Sezanas-MacBook-Air.local>
2021-05-18 18:42:18 +0200

    Task 5 - adding animal

--- src/components/AnimalsList.vue
+++ src/components/AnimalsList.vue
@@ -3,15 +3,42 @@
     <h1>
       Animals
     </h1>
-    <ul>
-      <li :key="index" v-for="(animal, index) in animals">
-        {{ animal.species }}
-        {{ animal.name }}
-        {{ animal.birthday ? animal.birthday : "Unknown" }}
+    <h4>Add Animal</h4>
+    <form @submit.prevent="addAnimal">
+      <div>
+        <label>
+          Species:
+        </label>
+        <input type="text" v-model="newSpecies" />
+      </div>
+      <div>
+        <label>
+          Name:
+        </label>
+        <input type="text" v-model="newName" />
+      </div>
+      <div>
+        <label>
+          Date of Birth:
+        </label>
+        <input type="text" v-model="newDateOfBirth" />
+      </div>
+      <button>Add Animal</button>
+    </form>
+    <table>
+      <tr>
+        <th>Species</th>
+        <th>Name</th>
+        <th>Date of Birth</th>
+      </tr>
+      <tr v-for="(animal, index) in animals" :key="index">
+        <td>{{ animal.species }}</td>
+        <td>{{ animal.name }}</td>
+        <td>{{ animal.dateOfBirth ? animal.dateOfBirth : "Unknown" }}</td>
         <button @click="removeFromList(index)">Remove</button>
         <button @click="moveToTop(index)">Move to top</button>
-      </li>
-    </ul>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -20,12 +47,15 @@ export default {
   name: "HelloWorld",
   data() {
     return {
+      newSpecies: "",
+      newName: "",
+      newDateOfBirth: "",
       animals: [
-        { species: "bird", name: "Paja", birthday: null },
-        { species: "dog", name: "Kala", birthday: new Date("2019") },
-        { species: "dog", name: "Kica", birthday: new Date("2018") },
-        { species: "cat", name: "Maza", birthday: new Date("2016") },
-        { species: "horse", name: "Suza", birthday: new Date("2002") },
+        { species: "bird", name: "Paja", dateOfBirth: null },
+        { species: "dog", name: "Kala", dateOfBirth: new Date("2019") },
+        { species: "dog", name: "Kica", dateOfBirth: new Date("2018") },
+        { species: "cat", name: "Maza", dateOfBirth: new Date("2016") },
+        { species: "horse", name: "Suza", dateOfBirth: new Date("2002") },
       ],
     };
   },
@@ -35,8 +65,18 @@ export default {
     },
     moveToTop(index) {
       const movingItem = this.animals.splice(index, 1);
-      const movingItemToTop = this.animals.unshift(movingItem[0]);
-      return movingItemToTop;
+      const movedItemToTop = this.animals.unshift(movingItem[0]);
+      return movedItemToTop;
+    },
+    addAnimal() {
+      this.animals.push({
+        species: this.newSpecies,
+        name: this.newName,
+        dateOfBirth: this.newDateOfBirth,
+      });
+      this.newSpecies = "";
+      this.newName = "";
+      this.newDateOfBirth = "";
     },
   },
 };
@@ -57,5 +97,11 @@ li {
 }
 a {
   color: #42b983;
+}
+table {
+  margin: 30px auto;
+}
+table td {
+  border: 1px solid grey;
 }
 </style>
